4ba14ffbce84206b9f8daf67665c5e4445428766
Daniel ZHOU <danielzhou@Daniels-MacBook-Air.local>
2022-07-21 23:17:49 -0400

    added feature to read multiple genes from file and compared them

--- HW_1/HW_1_biopython.py
+++ HW_1/HW_1_biopython.py
@@ -1,16 +1,39 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqFeature import SeqFeature,FeatureLocation
-record = SeqIO.read("sequence.gb", "gb")
-for f in record.features:
-    if f.type == "CDS":
-        gene_seq = Seq(record.seq)
-        gene_sub_seq= f.location.extract(gene_seq)
-        calculated_translation_string = gene_sub_seq.translate()
-        for item in f.qualifiers:
-            if item == "translation":
-                # + "*" because extract() function always returns a string ending with "*"
-                translation_string = f.qualifiers[item][0] + "*"
-        if calculated_translation_string == translation_string:
-            print("vamos")
-        break
+from Bio import Entrez
+
+Entrez.email = 'danielzh08@gmail.com'
+
+# open genes.txt file containing one gene per line
+with open('genes.txt') as gene_file:
+    for gene_type in gene_file:
+        
+        # serach genbank for gene ID
+        handle = Entrez.esearch(db = "nuccore", term = gene_type)
+        record = Entrez.read(handle)
+        gi_list = record["IdList"]
+        gi_str = ",".join(gi_list)
+
+        print(gi_str)
+
+        # get genebank file in gb format
+        handle = Entrez.efetch(db = "nuccore", id = gi_str, rettype = "gb", retmode = "text")
+        records = SeqIO.parse(handle, "gb")
+
+        for record in records:
+            #record = SeqIO.read("sequence.gb", "gb")
+            for f in record.features:
+
+                # find all CDS types 
+                # do nucleotide translation and comparison
+                if f.type == "CDS":
+                    gene_seq = Seq(record.seq)
+                    gene_sub_seq = f.location.extract(gene_seq)
+                    calculated_translation_string = gene_sub_seq.translate()
+                    # + "*" because extract() function always returns a string ending with "*"
+                    translation_string = f.qualifiers["translation"][0] + "*"
+                    if calculated_translation_string == translation_string:
+                        print(f'{f.qualifiers["protein_id"][0]} Translation results match')
+                    else:
+                        print(f'{f.qualifiers["protein_id"][0]} Translation results do not match')
